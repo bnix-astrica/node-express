ad1b54761125b06bc5e362bf59f5423902f1d8af
Bruce Nix <bnix@astrica.ai>
2026-06-11 16:05:20 -0500

    Node Express Excercise

--- config/index.js
+++ config/index.js
@@ -1,4 +1,8 @@
 import dotenv from "dotenv";
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
 
 // ensures that env variables are loaded
 const envFound = dotenv.config();
@@ -7,8 +11,38 @@ if (!envFound) {
   throw new Error("Couldn't find .env!");
 }
 
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  try {
+    res.status(200);
+    res.send("Hello World!");
+  } catch (error) {
+    res.status(500);
+    res.send("A server error occurred while handling your request.");
+  }
+});
+
+//seteup  GET request handler
+app.get("/", (req, res) => {
+  try {
+    res.status(200).json({
+      message: "Hello World!",
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "An error occurred",
+    });
+  }
+});
+
+//set server to listen on port defined in .env file
+//app.listen(3000, () => console.log("Server listening on port 3000..."));
+
 // exports env variables for use
 export default {
+  app,
   mysql: {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
